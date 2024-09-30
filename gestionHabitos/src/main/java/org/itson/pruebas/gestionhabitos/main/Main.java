777d90de8d61f848f4592d15bcf753b15930b300
jssroberto <rovertyto@outlook.com>
2024-09-29 23:28:08 -0700

    Navegación con JPanels
    Se modificó la forma de navegación entre interfaces

---
+++ gestionHabitos/src/main/java/org/itson/pruebas/gestionhabitos/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.pruebas.gestionhabitos.main;
+
+import org.itson.pruebas.gestionhabitos.view.FrameContenedor;
+
+/**
+ *
+ * @author rover
+ */
+public class Main {
+    public static void main(String[] args) {
+        FrameContenedor frameContenedor = new FrameContenedor();
+        frameContenedor.mostrarInicioSesion();
+        frameContenedor.setVisible(true);
+    }
+}
