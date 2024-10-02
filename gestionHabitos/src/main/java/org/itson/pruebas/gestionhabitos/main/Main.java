7a06b98165e51a44a1f8f189cad20d28eb87c04f
jssroberto <rovertyto@outlook.com>
2024-10-01 22:32:53 -0700

    Interfaz Inicio lista
    Se terminó de implementar el diseño de la interfaz de inicio. Falta integrarse

--- gestionHabitos/src/main/java/org/itson/pruebas/gestionhabitos/main/Main.java
+++ gestionHabitos/src/main/java/org/itson/pruebas/gestionhabitos/main/Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.itson.pruebas.gestionhabitos.main;
 
 import org.itson.pruebas.gestionhabitos.view.FrameContenedor;
@@ -13,7 +9,7 @@ import org.itson.pruebas.gestionhabitos.view.FrameContenedor;
 public class Main {
     public static void main(String[] args) {
         FrameContenedor frameContenedor = new FrameContenedor();
-        frameContenedor.mostrarInicioSesion();
+        frameContenedor.mostrarInicio();
         frameContenedor.setVisible(true);
     }
 }
